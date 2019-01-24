ef38e8c33c0524d5b8bafc719258eba6299c4c16
j3432p <j3432p@Johnny.local>
2019-01-23 22:45:27 -0500

    divided logic into seperate components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,18 +6,6 @@ import 'whatwg-fetch';
 
 
 class App extends Component {
-  state = {
-    series: []
-  }
-
-      
-componentDidMount() {
-  fetch('http://api.tvmaze.com/search/shows?q=Vikings') 
-  .then(response => response.json())
-  .then(json => this.setState({series: json}))
-
-}
-
   render() {
     return (
       <div className="App">
@@ -25,7 +13,6 @@ componentDidMount() {
           <h1 className="App-title"> Tv Series List </h1>
         </header>
        <Intro message="Here you can find all of your most loved series" />   
-       The length of series array - {this.state.series.length}      
        <Series />
       </div>
     );
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 
 
 class Series extends Component {
+  state = {
+    series: []
+  }
+
+
+  componentDidMount() {
+    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+      .then(response => response.json())
+      .then(json => this.setState({ series: json }));
+
+  }
   render() {
     return (
-      <div> Series container</div>
+      <div>The length of series array - {this.state.series.length}      
+</div>
     )
   }
 }
