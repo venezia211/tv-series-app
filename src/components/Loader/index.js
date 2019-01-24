978ec9464cdb699e8b9581e872a188617f7be77a
j3432p <j3432p@Johnny.local>
2019-01-24 00:14:46 -0500

    added img loader

---
+++ src/components/Loader/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import loaderSrc from '../../asset/Pacman.gif'
+
+const Loader = props => (
+  <div> 
+    <img
+    style = {{width: 75}}
+    alt = 'Loader icon'
+    src = {loaderSrc} />
+
+
+  </div>
+);
+
+export default Loader;
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SeriesList from '../../components/SeriesList';
+import Loader from '../../components/Loader';
 
 class Series extends Component {
   state = {
@@ -31,17 +32,17 @@ class Series extends Component {
            onChange={this.onSeriesInputChange} />
         </div>
         {
-          series.length === 0 && seriesName.trim() === ''
+          !isFetching && series.length === 0 && seriesName.trim() === ''
            && 
           <p> Please enter series name into the input </p> 
         }
         {
-          series.length === 0 && seriesName.trim() !== ''
+          !isFetching && series.length === 0 && seriesName.trim() !== ''
           && 
           <p> No TV series found with this name </p>
         }
         {
-          isFetching && <p>Loading...</p>
+          isFetching && <Loader />
         }
         {
           isFetching && <SeriesList list={this.state.series} />    
