d0e883a29714a66f39afe5b28a219b45edb4cecf
j3432p <j3432p@Johnny.local>
2019-01-23 23:27:44 -0500

    new component

---
+++ src/components/SeriesList/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+
+
+const SeriesListItem = ({ series }) => (
+  <li key={series.show.id}>
+    {series.show.name}
+  </li>
+)
+ 
+
+const SeriesList = (props) => {
+  return (
+    <div> 
+      <ul className='series-list'>
+        {props.list.map(series => (
+          <SeriesListItem series = {series}/>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default SeriesList;
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import SeriesList from '../../components/SeriesList';
 
 class Series extends Component {
   state = {
@@ -15,7 +15,8 @@ class Series extends Component {
   }
   render() {
     return (
-      <div>The length of series array - {this.state.series.length}      
+      <div>The length of series array - {this.state.series.length}  
+      <SeriesList list= {this.state.series} />    
 </div>
     )
   }
